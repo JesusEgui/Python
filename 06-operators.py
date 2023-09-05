1eb5422fd6744980232c37b066ae82371f9c3efe
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:41:13 -0400

    Create 06-Operators.py

---
+++ 06-operators.py
@@ -0,0 +1,25 @@
+Exercise 1:
+
+# Exercise 1: Calculate the area of a rectangle with given length and width.
+
+length = 8
+width = 5
+
+# Calculate the area of the rectangle and store it in the 'area' variable.
+area = length * width
+
+# Print the result.
+print("The area of the rectangle is:", area)
+Exercise 2:
+
+Exercise 2: Calculate the total cost of buying multiple items.
+
+item_price = 12  # Price of a single item
+quantity = 10    # Number of items to purchase
+tax_rate = 0.08  # Sales tax rate (8%)
+
+# Calculate the total cost, including tax, and store it in the 'total_cost' variable.
+total_cost = (item_price * quantity) * (1 + tax_rate)
+
+# Print the result.
+print("The total cost, including tax, is:", total_cost)
