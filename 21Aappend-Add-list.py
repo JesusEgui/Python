053fe30eb813449e7132d3a81757383e4d992b18
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 13:17:10 -0400

    Create 21Aappend-Add-list.py

---
+++ 21Aappend-Add-list.py
@@ -0,0 +1,15 @@
+movie_ratings = [8, 7, 6, 9, 8]
+movie_ratings.append(7)
+print(movie_ratings)
+
+movie_ratings = [8, 7, 6, 9, 8]
+movie_ratings.insert(2, 5)
+print(movie_ratings)
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.append('orange')
+print(fruits)
+
+programming_languages = ['Python', 'Java', 'C++']
+programming_languages.append('JavaScript')
+print(programming_languages)
