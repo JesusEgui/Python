2b96f1e97c5fadfebdfc81f5c6b57a3ba94d5427
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 12:29:30 -0400

    Create 16-for-loop.py

---
+++ 16-for-loop.py
@@ -0,0 +1,17 @@
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+for fruit in fruits:
+    print('Current fruit:', fruit)
+  -------------------------------------
+  
+for number in range(2, 21, 2):
+    print('Even number:', number)
+  --------------------------------------
+
+for letter in 'hello!':
+    print('Current letter:', letter)
+    
+
+for counter in range(1,11):
+    print(counter)
+print('Finished!')
