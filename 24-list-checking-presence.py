1f70c7fc22aa4b1b6c055f07c1cd5d5b187f41fb
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 13:23:26 -0400

    Create 24-list-checking-presence.py

---
+++ 24-list-checking-presence.py
@@ -0,0 +1,39 @@
+Iterating through a string and printing each character:
+
+for char in 'exciting adventure':
+    print(char)
+Checking if a name is in a list of invited guests:
+
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name? ')
+if name in invited_guests:
+    print('Welcome!')
+else:
+    print('You are not invited!')
+Checking if a name is not in a list of invited guests:
+
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name? ')
+if name not in invited_guests:
+    print('You are not invited!')
+else:
+    print('Welcome!')
+
+for char in 'happy message':
+    print(char)
+    
+    
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name?')
+if name in invited_guests:
+    print('Welcome!')
+else:
+    print('You are not invited!')
+    
+    
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name?')
+if name not in invited_guests:
+    print('You are not invited!')
+else:
+    print('Welcome!')
