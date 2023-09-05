2a88b5c9afff4151ef28eae5e4cf7cb363a51357
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:45:55 -0400

    Create 08-Input.py

---
+++ 08-Input.py
@@ -0,0 +1,18 @@
+# Exercise 1: Ask the user for their age and print a message.
+
+age = input("How old are you? ")
+print("You are", age, "years old.")
+Exercise 2:
+
+python
+Copy code
+# Exercise 2: Ask the user for their favorite color and print a message.
+
+favorite_color = input("What is your favorite color? ")
+print("Your favorite color is", favorite_color + ".")
+
+
+
+
+
+
