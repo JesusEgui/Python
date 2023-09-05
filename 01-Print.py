1a696a8297823e94238858e13d4b42fdc30933da
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:16:49 -0400

    01-Print.py

--- 01-Print.py
+++ 01-Print.py
@@ -1,2 +1,11 @@
+print("Python's simplicity and power make it a fantastic choice!")
+print("I'm proud to be on this Python journey!")
+
+print("Becoming a Python developer opens up many opportunities!")
+print("I'm ready for the challenges ahead!")
+
+print("Python programming is both fun and rewarding!")
+print("I'm enjoying every moment of it!")
+
 print ('I\'m learning to become a Python developer!')
 print ('I\'m so excited!')
