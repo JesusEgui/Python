7645706f3033c0a4aab2369af2d1816a3ad352df
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 12:34:58 -0400

    Create 19-lists.py

---
+++ 19-lists.py
@@ -0,0 +1,14 @@
+Adding a city to the end of the list:
+
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+new_city = 'San Francisco'
+top_cities.append(new_city)
+print(top_cities)
+
+Inserting a city at a specific position in the list:
+
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+new_city = 'Miami'
+position = 2  # Insert at the third position (position 2 in the list)
+top_cities.insert(position, new_city)
+print(top_cities)
