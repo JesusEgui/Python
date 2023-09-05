8a13aac74c58371ea7c03bf18817e98dca18bcbc
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:39:53 -0400

    Create 05-Numerical-Representations.py

---
+++ 05-Numerical-Representations.py
@@ -0,0 +1,20 @@
+Exercise 1:
+
+# Exercise 1: Convert the hexadecimal number to decimal.
+
+# Hexadecimal number starting with 0X or 0x
+hex_number = 0x1A7F
+
+# Convert the hex_number to decimal and print the result.
+decimal_result = int(hex_number)
+print(decimal_result)
+Exercise 2:
+
+# Exercise 2: Convert the octal number to decimal.
+
+# Octal number starting with 0O or 0o
+octal_number = 0o7654
+
+# Convert the octal_number to decimal and print the result.
+decimal_result = int(octal_number)
+print(decimal_result)
