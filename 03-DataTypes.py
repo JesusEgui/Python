509e9fd619ec2b2cbac1cf2080118bc6b4baca70
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:33:22 -0400

    Create 03-DataTypes.py

---
+++ 03-DataTypes.py
@@ -0,0 +1,29 @@
+string
+favorite_color = 'blue'
+
+integer
+number_of_pets = 3
+
+float
+pi_value = 3.14159
+
+boolean
+
+is_sunny_today = True
+is_raining = False
+
+string
+favorite_movie = 'The Shawshank Redemption'
+
+integer
+years_of_experience = 10
+
+integer
+years_of_experience = 10
+
+float
+temperature_celsius = 22.5
+boolean
+
+is_hungry = True
+has_finished_homework = False
