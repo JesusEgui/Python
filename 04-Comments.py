b4af279f6812cea4c2e446db988b851c54604fa7
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:38:25 -0400

    Create 04-Comments.py

---
+++ 04-Comments.py
@@ -0,0 +1,18 @@
+Exercise 1:
+
+# Exercise 1: Fix the code to print the sum of var1 and var2 as integers.
+
+# Note that the variables are strings, not numbers!
+var1 = '23'
+var2 = '54'
+
+# Convert var1 and var2 to integers and print their sum.
+sum_result = int(var1) + int(var2)
+print(sum_result)
+Exercise 2:
+
+# Exercise 2: Debug the code to print the hidden message.
+
+print('hello')
+# print('hidden message') <- This line should not be commented out.
+print('world')  # Add a new line to print 'world'.
