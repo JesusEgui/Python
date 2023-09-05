1311fcd48debe43ef4d79e81dc1c2e9a1c3adf81
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 11:43:45 -0400

    Create 07-Reassigning-Values.py

---
+++ 07-Reassigning-Values.py
@@ -0,0 +1,32 @@
+Exercise 1:
+
+# Exercise 1: Calculate the total cost of purchasing multiple items.
+
+item_price = 15  # Price of a single item
+quantity = 4     # Number of items to purchase
+
+# Calculate the total cost and store it in the 'total_cost' variable.
+total_cost = item_price * quantity
+
+# Print the result.
+print("The total cost of purchasing", quantity, "items is:", total_cost)
+Solution 1:
+
+The total cost of purchasing 4 items is: 60
+
+
+Exercise 2:
+
+# Exercise 2: Calculate the area of a triangle.
+
+base_length = 10  # Length of the base of the triangle
+height = 6         # Height of the triangle
+
+# Calculate the area of the triangle and store it in the 'area' variable.
+area = 0.5 * base_length * height
+
+# Print the result.
+print("The area of the triangle is:", area)
+Solution 2:
+
+The area of the triangle is: 30.0
